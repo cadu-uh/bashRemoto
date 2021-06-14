bea431cbb239ad4aee9ad55b051308f4bb0f6823
Carlos Eduardo <carlos.eduardo1801@hotmail.com>
2021-06-14 16:43:06 -0300

    Commite master

--- src/teste/TesTe.java
+++ src/teste/TesTe.java
@@ -1,4 +1,3 @@
-
 package teste;
 
 /**
@@ -6,25 +5,30 @@ package teste;
  * @author Carlos
  */
 public class TesTe {
-    
-    public static void testando (int m){
-        System.out.println("    Okdbfkdbfkds");
-}
 
+    public static void testando(int m) {
+        System.out.println("    Okdbfkdbfkds");
+    }
 
     public static int retorna(int num) {
         return num;
     }
 
     public static void main(String[] args) {
-      System.out.println("Ola Mundo");
+        System.out.println("Ola Mundo");
 
         int i = retorna(33);
         for (int j = 0; j < 5; j++) {
 
             System.out.println(j + i);
-          
+ 
 
         }
+        
+        for (int j = 0; j < 10; j++) {
+            System.out.println(i);
+            
+        }
 
+    }
 }
